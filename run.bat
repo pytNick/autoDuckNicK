2ed6e943eb71b8dadff6c1fb5ba536d0c8122289
pytNick <nt@sippicom.com>
2021-02-10 15:24:56 +0100

    SCHULE

--- run.bat
+++ run.bat
@@ -2,4 +2,6 @@
 echo lalalalalalalalalalalalalalalala  
 echo nanananananananana
 echo blublublublblbublbublbk
+echo pupupupuppupupupupupupp   
+echo Nick muss nächste woch e in die Scfhule und das ist nicht gut..
 pause
