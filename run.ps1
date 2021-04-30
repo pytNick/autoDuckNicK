74d7f1a1ffbc27e1fa6a72bc9183baade68175be
pytNick <78860045+pytNick@users.noreply.github.com>
2021-04-30 15:12:05 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,2 +1,11 @@
-write-host test123
-pause
+Get-PSDrive -PSProvider Registry
+
+Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -value "0"
+Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -value "1"
+Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -value "1"
+Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -value "1"
+
+Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Classes\CLSID\`{031E4825-7B94-4dc3-B131-E946B44C8DD5`} -Name System.IsPinnedToNameSpaceTree -value "1"
+
+taskkill /f /im explorer.exe
+start explorer.exe
