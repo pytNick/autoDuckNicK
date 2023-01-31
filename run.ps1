6c4ba4bef96687b95fda850758909cf9674dc7eb
pytNick <78860045+pytNick@users.noreply.github.com>
2023-01-31 16:22:12 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,11 +1,8 @@
-Get-PSDrive -PSProvider Registry
-
-Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -value "0"
-Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -value "1"
-Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -value "1"
-Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -value "1"
-
-Set-ItemProperty -Type DWord -Path HKCU:\SOFTWARE\Classes\CLSID\`{031E4825-7B94-4dc3-B131-E946B44C8DD5`} -Name System.IsPinnedToNameSpaceTree -value "1"
-
-taskkill /f /im explorer.exe
-start explorer.exe
+$imgURL = "https://imgur.com/TUwv8kH.jpg"
+			Invoke-WebRequest -Uri $imgURL -OutFile $env:TEMP\PSLockScreenWallpaper.jpg
+			Copy-Item $env:TEMP\PSLockScreenWallpaper.jpg "C:\Windows\System32\LockScreen.jpg" -Force
+			Write-Host "Creating registry path $("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP")."
+			New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP" -Force | Out-Null
+			New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP" -Name "LockScreenImageStatus" -Value "1" -PropertyType DWORD -Force | Out-Null
+        	New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP" -Name "LockScreenImagePath" -Value C:\Windows\System32\LockScreen.jpg -PropertyType STRING -Force | Out-Null
+        	New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP" -Name "LockScreenImageUrl" -Value C:\Windows\System32\LockScreen.jpg -PropertyType STRING -Force | Out-Null
